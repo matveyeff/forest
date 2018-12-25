6609815517cbab83cb638a542417583974f8783e
Dmitrii Matveev <matveyeff@mail.ru>
2018-12-25 16:25:37 +0300

    flip card added

--- src/assets/scripts/auth.js
+++ src/assets/scripts/auth.js
@@ -1,1 +1,12 @@
 console.log("auth module loaded");
+
+var authBtn = document.getElementById('authBtn');
+var signInBack = document.getElementById('sign_in_back')
+
+authBtn.addEventListener('click', function() {
+	document.getElementById('flipper').style.transform = 'rotateY(180deg)';
+})	
+
+signInBack.addEventListener('click', function() {
+	document.getElementById('flipper').style.transform = 'rotateY(360deg)';
+})
